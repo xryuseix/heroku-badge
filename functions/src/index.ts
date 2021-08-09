2d20f6ab7abd27e4b568963cf67a24173bcfd6e0
xryuseix <ryusei143.shootingstar@gmail.com>
2021-08-09 20:41:59 +0900

    feat: バッジを作成する機能追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,34 @@ import * as express from "express";
 const app = express();
 app.use(express.static("public"));
 
-app.get("/badge", (req, res) => {
-  //   res.send(req.query);
-  const appName = "NEET";
-  const status = "Activate";
-  const badgeColor = "624888";
-  res.redirect(
-    `https://img.shields.io/badge/${appName}-${status}-${badgeColor}?logo=heroku`
-  );
+app.get("/badge", async (req, res) => {
+  const fetch = require("node-fetch");
+  const appName = req.query.app as string | undefined;
+  if (typeof appName !== "undefined") {
+    try {
+      const response = await fetch(`https://${appName}.herokuapp.com`);
+      const activeFlag: boolean = response.ok;
+      const status: string = activeFlag ? "Activate" : "Inactivate";
+      const badgeColor: string = activeFlag ? "624888" : "D35C46";
+      res.redirect(
+        `https://img.shields.io/badge/${appName.replace(
+          "-",
+          "_"
+        )}-${status}-${badgeColor}?logo=heroku`
+      );
+    } catch (err) {
+      // http://localhost:5000/badge?app
+      // http://localhost:5000/badge?app=
+      res.redirect(
+        `https://img.shields.io/badge/404-param_is_undefined-D35C46?logo=heroku`
+      );
+    }
+  } else {
+    // http://localhost:5000/badge
+    res.redirect(
+      `https://img.shields.io/badge/404-param_is_undefined-D35C46?logo=heroku`
+    );
+  }
 });
 
 exports.app = functions.https.onRequest(app);
