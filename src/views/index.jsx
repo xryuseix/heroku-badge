4786bc8f561e59c4c2d9f5e366a8ebe36206c248
xryuseix <ryusei143.shootingstar@gmail.com>
2021-08-11 00:20:11 +0900

    refactor: きれいにした

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -2,14 +2,47 @@
 
 const React = require("react");
 
+
+const Apps = [
+  {
+    url: "https://heroku-booting-badge.herokuapp.com/badge?app=heroku-booting-badge",
+    alt: "heroku-booting-badge",
+  },
+  {
+    url: "https://heroku-booting-badge.herokuapp.com/badge?app=non_existent_apps",
+    alt: "non-existent-apps",
+  },
+  {
+    url: "https://heroku-booting-badge.herokuapp.com/badge",
+    alt: "No param",
+  },
+  {
+    url: "https://heroku-booting-badge.herokuapp.com/badge?app",
+    alt: "Incorrect param",
+  },
+  {
+    url: "https://heroku-booting-badge.herokuapp.com/badge?app=",
+    alt: "Incorrect param",
+  },
+];
+
 const indexPage = () => {
   return (
     <div>
+      <head>
+        <link rel="stylesheet" href="./index.css" />
+      </head>
       <h2>Welcome to Heroku badges! (unofficial)</h2>
       <p>
         This program checks the working status of
-        <a href="https://heroku.com/home">Heroku</a> and generates the URL of
-        <a href="https://shields.io/">shieils.io</a>.
+        <a
+          href="https://heroku.com/home"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Heroku
+        </a>
+        .
       </p>
       <h2>Usage</h2>
       <p>
@@ -19,79 +52,24 @@ const indexPage = () => {
           {"{"}YOUR HEROKU APP NAME{"}"})
         </code>
       </p>
-      <table>
+      <table className="index_table">
         <thead>
           <tr>
-            <th style={{textAlign: "left"}}>Example URL</th>
-            <th style={{textAlign: "center"}}>Badge</th>
+            <th className="index_url">Example URL</th>
+            <th className="index_badge">Badge</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td style={{textAlign: "left"}}>
-              <a href="https://heroku-booting-badge.herokuapp.com/badge?app=heroku-booting-badge">
-                https://heroku-booting-badge.herokuapp.com/badge?app=heroku-booting-badge
-              </a>
-            </td>
-            <td style={{textAlign: "center"}}>
-              <img
-                src="https://heroku-booting-badge.herokuapp.com/badge?app=heroku-booting-badge"
-                alt="neet-obserber"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td style={{textAlign: "left"}}>
-              <a href="https://heroku-booting-badge.herokuapp.com/badge?app=non-existent-apps">
-                https://heroku-booting-badge.herokuapp.com/badge?app=non-existent-apps
-              </a>
-            </td>
-            <td style={{textAlign: "center"}}>
-              <img
-                src="https://heroku-booting-badge.herokuapp.com/badge?app=non-existent-apps"
-                alt="non-existent-apps"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td style={{textAlign: "left"}}>
-              <a href="https://heroku-booting-badge.herokuapp.com/badge">
-                https://heroku-booting-badge.herokuapp.com/badge
-              </a>
-            </td>
-            <td style={{textAlign: "center"}}>
-              <img
-                src="https://heroku-booting-badge.herokuapp.com/badge"
-                alt="No param"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td style={{textAlign: "left"}}>
-              <a href="https://heroku-booting-badge.herokuapp.com/badge?app">
-                https://heroku-booting-badge.herokuapp.com/badge?app
-              </a>
-            </td>
-            <td style={{textAlign: "center"}}>
-              <img
-                src="https://heroku-booting-badge.herokuapp.com/badge?app"
-                alt="Incorrect param"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td style={{textAlign: "left"}}>
-              <a href="https://heroku-booting-badge.herokuapp.com/badge?app=">
-                https://heroku-booting-badge.herokuapp.com/badge?app=
-              </a>
-            </td>
-            <td style={{textAlign: "center"}}>
-              <img
-                src="https://heroku-booting-badge.herokuapp.com/badge?app="
-                alt="Incorrect param"
-              />
-            </td>
-          </tr>
+          {Apps.map((data) => (
+            <tr>
+              <td className="index_url">
+                <a href={data.url}>{data.url}</a>
+              </td>
+              <td className="index_badge">
+                <img src={data.url} alt={data.alt} />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
